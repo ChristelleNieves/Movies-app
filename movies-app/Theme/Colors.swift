723de4f26964487744bf8d987c822cfec48718d6
Christelle Nieves <christellejnieves@gmail.com>
2021-06-12 14:02:32 -0400

    added colors

---
+++ movies-app/Theme/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  movies-app
+//
+//  Created by Christelle Nieves on 6/12/21.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    static var red = UIColor.init(red: 237/240.0, green: 0/240.0, blue: 38/240.0, alpha: 0.87)
+}
--- movies-app/View Controllers/MovieListViewController.swift
+++ movies-app/View Controllers/MovieListViewController.swift
@@ -82,7 +82,7 @@ extension MovieListViewController {
     }
     
     func setupNavController() {
-        navigationController?.navigationBar.barTintColor = UIColor.init(red: 237/240.0, green: 0/240.0, blue: 38/240.0, alpha: 0.87)
+        navigationController?.navigationBar.barTintColor = Colors.red
     }
     
     func setupTableView() {
