071679b349d758ca7b3d8c6d7a30d5e3e0e8d333
Christelle Nieves <christellejnieves@gmail.com>
2021-06-13 03:17:12 -0400

    ui changes

--- movies-app/View Controllers/Home Screen/MainViewController.swift
+++ movies-app/View Controllers/Home Screen/MainViewController.swift
@@ -62,13 +62,13 @@ extension MainViewController {
     }
     
     func setupNav() {
-        navigationController?.navigationBar.tintColor = Colors.red
+        navigationController?.navigationBar.tintColor = Colors.purple
         navigationController?.navigationBar.backItem?.backBarButtonItem?.tintColor = .black
     }
     
     func setupTodayLabel() {
         todayLabel.text = "Trending Today"
-        todayLabel.font = UIFont.systemFont(ofSize: 25)
+        todayLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 35, weight: .light)
         todayLabel.textColor = UIColor.init(white: 1, alpha: 0.60)
         view.addSubview(todayLabel)
         
@@ -77,14 +77,17 @@ extension MainViewController {
         
         NSLayoutConstraint.activate([
             todayLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
-            todayLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20)
+            todayLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5)
         ])
     }
     
     func setupTodayMoviesButton() {
         todayMoviesButton.setTitle("Movies", for: .normal)
-        todayMoviesButton.backgroundColor = Colors.red.withAlphaComponent(0.77)
+        todayMoviesButton.backgroundColor = .black
         todayMoviesButton.layer.cornerRadius = 12
+        todayMoviesButton.layer.borderColor = Colors.purple.cgColor
+        todayMoviesButton.layer.borderWidth = 1
+        todayMoviesButton.alpha = 0.77
         todayMoviesButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
         view.addSubview(todayMoviesButton)
         
@@ -100,14 +103,19 @@ extension MainViewController {
         
         NSLayoutConstraint.activate([
             todayMoviesButton.topAnchor.constraint(equalTo: todayLabel.bottomAnchor, constant: 15),
-            todayMoviesButton.leadingAnchor.constraint(equalTo: todayLabel.leadingAnchor, constant: 20)
+            todayMoviesButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
+            todayMoviesButton.widthAnchor.constraint(equalToConstant: 175),
+            todayMoviesButton.heightAnchor.constraint(equalToConstant: 70)
         ])
     }
     
     func setupTodayShowsButton() {
         todayShowsButton.setTitle("TV Shows", for: .normal)
-        todayShowsButton.backgroundColor = Colors.red.withAlphaComponent(0.77)
+        todayShowsButton.backgroundColor = .black
         todayShowsButton.layer.cornerRadius = 12
+        todayShowsButton.layer.borderWidth = 1
+        todayShowsButton.layer.borderColor = Colors.purple.cgColor
+        todayShowsButton.alpha = 0.77
         todayShowsButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
         view.addSubview(todayShowsButton)
         
@@ -122,13 +130,15 @@ extension MainViewController {
         
         NSLayoutConstraint.activate([
             todayShowsButton.topAnchor.constraint(equalTo: todayMoviesButton.topAnchor),
-            todayShowsButton.leadingAnchor.constraint(equalTo: todayMoviesButton.trailingAnchor, constant: 10)
+            todayShowsButton.leadingAnchor.constraint(equalTo: todayMoviesButton.trailingAnchor, constant: 10),
+            todayShowsButton.widthAnchor.constraint(equalToConstant: 175),
+            todayShowsButton.heightAnchor.constraint(equalToConstant: 70)
         ])
     }
     
     func setupMoviesLabel() {
         moviesLabel.text = "Movies"
-        moviesLabel.font = UIFont.systemFont(ofSize: 25)
+        moviesLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 35, weight: .light)
         moviesLabel.textColor = UIColor.init(white: 1, alpha: 0.60)
         view.addSubview(moviesLabel)
         
@@ -136,14 +146,14 @@ extension MainViewController {
         moviesLabel.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            moviesLabel.topAnchor.constraint(equalTo: todayShowsButton.bottomAnchor, constant: 50),
-            moviesLabel.leadingAnchor.constraint(equalTo: todayLabel.leadingAnchor)
+            moviesLabel.topAnchor.constraint(equalTo: todayShowsButton.bottomAnchor, constant: 30),
+            moviesLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5)
         ])
     }
     
     func setupNowPlayingButton() {
         nowPlayingButton.setTitle("Now Playing", for: .normal)
-        nowPlayingButton.layer.borderColor = Colors.red.cgColor
+        nowPlayingButton.layer.borderColor = Colors.purple.cgColor
         nowPlayingButton.layer.borderWidth = 1
         nowPlayingButton.layer.cornerRadius = 15
         nowPlayingButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
@@ -161,13 +171,15 @@ extension MainViewController {
         
         NSLayoutConstraint.activate([
             nowPlayingButton.topAnchor.constraint(equalTo: moviesLabel.bottomAnchor, constant: 15),
-            nowPlayingButton.leadingAnchor.constraint(equalTo: moviesLabel.leadingAnchor, constant: 20)
+            nowPlayingButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
+            nowPlayingButton.widthAnchor.constraint(equalToConstant: 175),
+            nowPlayingButton.heightAnchor.constraint(equalToConstant: 70)
         ])
     }
     
     func setupUpcomingMoviesButton() {
         upcomingMoviesButton.setTitle("Upcoming", for: .normal)
-        upcomingMoviesButton.layer.borderColor = Colors.red.cgColor
+        upcomingMoviesButton.layer.borderColor = Colors.purple.cgColor
         upcomingMoviesButton.layer.borderWidth = 1
         upcomingMoviesButton.layer.cornerRadius = 15
         upcomingMoviesButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
@@ -185,13 +197,15 @@ extension MainViewController {
         
         NSLayoutConstraint.activate([
             upcomingMoviesButton.topAnchor.constraint(equalTo: nowPlayingButton.topAnchor),
-            upcomingMoviesButton.leadingAnchor.constraint(equalTo: nowPlayingButton.trailingAnchor, constant: 10)
+            upcomingMoviesButton.leadingAnchor.constraint(equalTo: nowPlayingButton.trailingAnchor, constant: 10),
+            upcomingMoviesButton.widthAnchor.constraint(equalToConstant: 175),
+            upcomingMoviesButton.heightAnchor.constraint(equalToConstant: 70)
         ])
     }
     
     func setupTopRatedMoviesButton() {
         topRatedMoviesButton.setTitle("Top Rated", for: .normal)
-        topRatedMoviesButton.layer.borderColor = Colors.red.cgColor
+        topRatedMoviesButton.layer.borderColor = Colors.purple.cgColor
         topRatedMoviesButton.layer.borderWidth = 1
         topRatedMoviesButton.layer.cornerRadius = 15
         topRatedMoviesButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
@@ -209,13 +223,15 @@ extension MainViewController {
         
         NSLayoutConstraint.activate([
             topRatedMoviesButton.topAnchor.constraint(equalTo: nowPlayingButton.bottomAnchor, constant: 15),
-            topRatedMoviesButton.leadingAnchor.constraint(equalTo: nowPlayingButton.leadingAnchor)
+            topRatedMoviesButton.leadingAnchor.constraint(equalTo: nowPlayingButton.leadingAnchor),
+            topRatedMoviesButton.widthAnchor.constraint(equalToConstant: 175),
+            topRatedMoviesButton.heightAnchor.constraint(equalToConstant: 70)
         ])
     }
     
     func setupPopularMoviesButton() {
         popularMoviesButton.setTitle("Popular", for: .normal)
-        popularMoviesButton.layer.borderColor = Colors.red.cgColor
+        popularMoviesButton.layer.borderColor = Colors.purple.cgColor
         popularMoviesButton.layer.borderWidth = 1
         popularMoviesButton.layer.cornerRadius = 15
         popularMoviesButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
@@ -233,14 +249,16 @@ extension MainViewController {
         
         NSLayoutConstraint.activate([
             popularMoviesButton.topAnchor.constraint(equalTo: topRatedMoviesButton.topAnchor),
-            popularMoviesButton.leadingAnchor.constraint(equalTo: topRatedMoviesButton.trailingAnchor, constant: 10)
+            popularMoviesButton.leadingAnchor.constraint(equalTo: topRatedMoviesButton.trailingAnchor, constant: 10),
+            popularMoviesButton.widthAnchor.constraint(equalToConstant: 175),
+            popularMoviesButton.heightAnchor.constraint(equalToConstant: 70)
         ])
         
     }
     
     func setupShowsLabel() {
         showsLabel.text = "TV Shows"
-        showsLabel.font = UIFont.systemFont(ofSize: 25)
+        showsLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 35, weight: .light)
         showsLabel.textColor = UIColor.init(white: 1, alpha: 0.60)
         view.addSubview(showsLabel)
         
@@ -248,14 +266,14 @@ extension MainViewController {
         showsLabel.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            showsLabel.topAnchor.constraint(equalTo: topRatedMoviesButton.bottomAnchor, constant: 50),
-            showsLabel.leadingAnchor.constraint(equalTo: moviesLabel.leadingAnchor)
+            showsLabel.topAnchor.constraint(equalTo: topRatedMoviesButton.bottomAnchor, constant: 30),
+            showsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5)
         ])
     }
     
     func setupTopRatedShowsButton() {
         topRatedShowsButton.setTitle("Top Rated", for: .normal)
-        topRatedShowsButton.layer.borderColor = Colors.red.cgColor
+        topRatedShowsButton.layer.borderColor = Colors.purple.cgColor
         topRatedShowsButton.layer.borderWidth = 1
         topRatedShowsButton.layer.cornerRadius = 15
         topRatedShowsButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
@@ -273,13 +291,15 @@ extension MainViewController {
         
         NSLayoutConstraint.activate([
             topRatedShowsButton.topAnchor.constraint(equalTo: showsLabel.bottomAnchor, constant: 15),
-            topRatedShowsButton.leadingAnchor.constraint(equalTo: nowPlayingButton.leadingAnchor)
+            topRatedShowsButton.leadingAnchor.constraint(equalTo: nowPlayingButton.leadingAnchor),
+            topRatedShowsButton.widthAnchor.constraint(equalToConstant: 175),
+            topRatedShowsButton.heightAnchor.constraint(equalToConstant: 70)
         ])
     }
     
     func setupPopularShowsButton() {
         popularShowsButton.setTitle("Popular", for: .normal)
-        popularShowsButton.layer.borderColor = Colors.red.cgColor
+        popularShowsButton.layer.borderColor = Colors.purple.cgColor
         popularShowsButton.layer.borderWidth = 1
         popularShowsButton.layer.cornerRadius = 15
         popularShowsButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
@@ -297,13 +317,15 @@ extension MainViewController {
         
         NSLayoutConstraint.activate([
             popularShowsButton.topAnchor.constraint(equalTo: topRatedShowsButton.topAnchor),
-            popularShowsButton.leadingAnchor.constraint(equalTo: topRatedShowsButton.trailingAnchor, constant: 10)
+            popularShowsButton.leadingAnchor.constraint(equalTo: topRatedShowsButton.trailingAnchor, constant: 10),
+            popularShowsButton.widthAnchor.constraint(equalToConstant: 175),
+            popularShowsButton.heightAnchor.constraint(equalToConstant: 70)
         ])
     }
     
     func setupAiringTodayButton() {
         airingTodayButton.setTitle("Airing Today", for: .normal)
-        airingTodayButton.layer.borderColor = Colors.red.cgColor
+        airingTodayButton.layer.borderColor = Colors.purple.cgColor
         airingTodayButton.layer.borderWidth = 1
         airingTodayButton.layer.cornerRadius = 15
         airingTodayButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
@@ -321,13 +343,15 @@ extension MainViewController {
         
         NSLayoutConstraint.activate([
             airingTodayButton.topAnchor.constraint(equalTo: topRatedShowsButton.bottomAnchor, constant: 15),
-            airingTodayButton.leadingAnchor.constraint(equalTo: topRatedShowsButton.leadingAnchor)
+            airingTodayButton.leadingAnchor.constraint(equalTo: topRatedShowsButton.leadingAnchor),
+            airingTodayButton.widthAnchor.constraint(equalToConstant: 175),
+            airingTodayButton.heightAnchor.constraint(equalToConstant: 70)
         ])
     }
     
     func setupCurrentlyAiringButton() {
         currentlyAiringButton.setTitle("Currently Airing", for: .normal)
-        currentlyAiringButton.layer.borderColor = Colors.red.cgColor
+        currentlyAiringButton.layer.borderColor = Colors.purple.cgColor
         currentlyAiringButton.layer.borderWidth = 1
         currentlyAiringButton.layer.cornerRadius = 15
         currentlyAiringButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
@@ -345,8 +369,9 @@ extension MainViewController {
         
         NSLayoutConstraint.activate([
             currentlyAiringButton.topAnchor.constraint(equalTo: airingTodayButton.topAnchor),
-            currentlyAiringButton.leadingAnchor.constraint(equalTo: airingTodayButton.trailingAnchor, constant: 10)
+            currentlyAiringButton.leadingAnchor.constraint(equalTo: airingTodayButton.trailingAnchor, constant: 10),
+            currentlyAiringButton.widthAnchor.constraint(equalToConstant: 175),
+            currentlyAiringButton.heightAnchor.constraint(equalToConstant: 70)
         ])
     }
-    
 }
