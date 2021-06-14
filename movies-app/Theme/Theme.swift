ee05ea22c3d85913baa6f9e3ccd3704dd35d9aba
Christelle Nieves <christellejnieves@gmail.com>
2021-06-13 22:41:08 -0400

    cleaning up

--- movies-app/Theme/Theme.swift
+++ movies-app/Theme/Theme.swift
@@ -9,18 +9,18 @@ import Foundation
 import UIKit
 
 struct Colors {
-    static var purple = UIColor.init(red: 84/240.0, green: 22/240.0, blue: 180/240.0, alpha: 0.87)
-    static var heading = UIColor.init(white: 1, alpha: 0.87)
+    static let purple = UIColor.init(red: 84/240.0, green: 22/240.0, blue: 180/240.0, alpha: 0.87)
+    static let heading = UIColor.init(white: 1, alpha: 0.87)
 }
 
 struct Fonts {
-    static var heading = UIFont.monospacedDigitSystemFont(ofSize: 35, weight: .light)
+    static let heading = UIFont.monospacedDigitSystemFont(ofSize: 35, weight: .light)
 }
 
 struct Alphas {
-    static var button = CGFloat(0.87)
+    static let button = CGFloat(0.87)
 }
 
 struct Insets {
-    static var button = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
+    static let button = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
 }
